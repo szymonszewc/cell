d121d7298375112d98074e9d51377ec7be6a1eef
szymonszewc <57077063+szymonszewc@users.noreply.github.com>
2019-11-04 19:55:15 +0100

    Add files via upload

---
+++ CellI2c/CellI2c/src/main.cpp
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+#define buzzer 9
+#define led 10
+#define interrupt 2
+#define DHT_PIN 3
+
+int Humidity;
+int Temperature;
+unsigned long acTime;
+unsigned long Time;
+unsigned long dTime;
+int n=0;
+DHT dht;
+LiquidCrystal_I2C lcd(0x27, 16,2);
+boolean i_want_to_see;
+void backlight()
+{
+  i_want_to_see=!i_want_to_see;
+}
+void setup() {
+  // put your setup code here, to run once:
+  dht.setup(DHT_PIN);
+  pinMode(interrupt,INPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(led,OUTPUT);
+  digitalWrite(buzzer,LOW);
+  digitalWrite(led,LOW);
+  lcd.init();
+  lcd.begin(16, 2);
+  lcd.display();
+  lcd.backlight();
+  i_want_to_see=1;
+attachInterrupt(digitalPinToInterrupt(interrupt), backlight, FALLING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (acTime<=4,294,967,295)
+  {
+  acTime=(millis()-(4,294,967,295*n));
+  dTime=acTime-Time;
+  if(dTime>=1000){
+  Humidity=dht.getHumidity();
+  Temperature=dht.getTemperature();
+   if (dht.getStatusString() == "OK") {
+     lcd.setCursor(0,0);
+     lcd.print(Humidity);
+     lcd.print("%");
+     lcd.setCursor(0,1);
+     lcd.print(Temperature);
+     lcd.print("*C");
+  if((Temperature>30)||(Humidity<60))
+    {
+     digitalWrite(buzzer,HIGH);
+     digitalWrite(led,HIGH);
+    }
+  else
+    {
+       digitalWrite(buzzer,LOW);
+       digitalWrite(led,LOW);
+    }
+   }  
+   Time=acTime;
+  }
+  if(i_want_to_see)
+  {
+    lcd.display();
+    lcd.backlight();
+  }
+  else
+  {
+    lcd.noDisplay();
+    lcd.noBacklight();
+  }
+}
+n++;
+}
